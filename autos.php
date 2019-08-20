11b7e1d8ed58479bf29241c9e59176e25f8a5605
Adam <ifone@me.com>
2019-08-20 15:56:51 -0400

    Add files via upload

--- autos.php
+++ autos.php
@@ -1,35 +1,58 @@
 <?php
+// Use pdo.php, assignment requirement
 require_once "pdo.php";
+
 $failure = false;
 $success = false;
-if (!isset($_GET['name'])) {
+
+//Testing for trying to get into autos.php without login - assignment requirement
+if (!isset($_GET['name']))
+{
     die("Name parameter missing");
-} elseif (isset($_POST['logout']) && $_POST['logout'] == 'Logout') {
+}
+
+//Test logging out and get redirect to index.php
+elseif (isset($_POST['logout']) && $_POST['logout'] == 'Logout')
+{
     header('Location: index.php');
-} elseif (isset($_POST['make']) && isset($_POST['year'])
-    && isset($_POST['mileage'])) {
-    if (!is_numeric($_POST['year']) || !is_numeric($_POST['mileage'])) {
-        $failure = 'Mileage and year must be numeric';
-    } elseif (strlen($_POST['make']) < 1 ) {
-        $failure = 'Make is required';
-    } else {
+}
+
+//Input Validation on mileage and year, message if is_numeric fails - assignment requirement
+
+elseif (isset($_POST['make']) && isset($_POST['year']) && isset($_POST['mileage']))
+    {
+    if (!is_numeric($_POST['year']) || !is_numeric($_POST['mileage']))
+        {
+          $failure = 'Mileage and year must be numeric';
+        }
+//Input validation on make (less than 1 character) - assignment requirement
+    elseif (strlen($_POST['make']) < 1 )
+        {
+          $failure = 'Make is required';
+        }
+//Validation has passed, inserting makem year and mileage into the database - assignment requirement
+    else
+        {
         $stmt = $pdo->prepare('INSERT INTO autos (make, year, mileage) VALUES ( :make, :year, :mileage)');
         $stmt->execute(array(
                 ':make' => $_POST['make'],
                 ':year' => $_POST['year'],
                 ':mileage' => $_POST['mileage'])
         );
+//Successfull Insert - assignment requirement
         $success = 'Record inserted';
-    }
-}
-$stmt = $pdo->query("SELECT make, year, mileage FROM autos");
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+      }
+
+        $stmt = $pdo->query("SELECT make, year, mileage FROM autos order by make asc");
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <?php require_once "bootstrap.php"; ?>
-    <title>Ziang Cui 's Login Page</title>
+    <title>Autos DB - Adam Hosa</title>
 </head>
 <body>
 <div class="container">
@@ -51,6 +74,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <input type="text" name="year"/></p>
         <p>Mileage:
             <input type="text" name="mileage"/></p>
+
         <input type="submit" value="Add">
         <input type="submit" name="logout" value="Logout">
     </form>
